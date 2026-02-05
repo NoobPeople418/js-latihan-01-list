c01796901910d2e2544ba51de73b026a67bf7d33
NoobPeople418 <syafiqnaufal973@gmail.com>
2026-02-05 10:10:30 +0700

    fix: minor

--- 08-list-daftar-karyawan.js
+++ 08-list-daftar-karyawan.js
@@ -12,16 +12,16 @@ function daftarKaryawan() {
     { nama: "Maya", posisi: "IT Support Specialist", gaji: 7500000, tahunMasuk: 2023 }
   ]
 
-  console.log("1.", listKaryawan[0].nama, "bekerja sebagai", listKaryawan[0].posisi);
-  console.log("2.", listKaryawan[1].nama, "bekerja sebagai", listKaryawan[1].posisi);
-  console.log("3.", listKaryawan[2].nama, "bekerja sebagai", listKaryawan[2].posisi);
-  console.log("4.", listKaryawan[3].nama, "bekerja sebagai", listKaryawan[3].posisi);
-  console.log("5.", listKaryawan[4].nama, "bekerja sebagai", listKaryawan[4].posisi);
-  console.log("6.", listKaryawan[5].nama, "bekerja sebagai", listKaryawan[5].posisi);
-  console.log("7.", listKaryawan[6].nama, "bekerja sebagai", listKaryawan[6].posisi);
-  console.log("8.", listKaryawan[7].nama, "bekerja sebagai", listKaryawan[7].posisi);
-  console.log("9.", listKaryawan[8].nama, "bekerja sebagai", listKaryawan[8].posisi);
-  console.log("10.", listKaryawan[9].nama, "bekerja sebagai", listKaryawan[9].posisi);
+  console.log("1.", listKaryawan[0].nama, "bekerja sebagai", listKaryawan[0].posisi, "gaji", listKaryawan[0].gaji, "tahun masuk", listKaryawan[0].tahunMasuk);
+  console.log("2.", listKaryawan[1].nama, "bekerja sebagai", listKaryawan[1].posisi, "gaji", listKaryawan[1].gaji, "tahun masuk", listKaryawan[1].tahunMasuk);
+  console.log("3.", listKaryawan[2].nama, "bekerja sebagai", listKaryawan[2].posisi, "gaji", listKaryawan[2].gaji, "tahun masuk", listKaryawan[2].tahunMasuk);
+  console.log("4.", listKaryawan[3].nama, "bekerja sebagai", listKaryawan[3].posisi, "gaji", listKaryawan[3].gaji, "tahun masuk", listKaryawan[3].tahunMasuk);
+  console.log("5.", listKaryawan[4].nama, "bekerja sebagai", listKaryawan[4].posisi, "gaji", listKaryawan[4].gaji, "tahun masuk", listKaryawan[4].tahunMasuk);
+  console.log("6.", listKaryawan[5].nama, "bekerja sebagai", listKaryawan[5].posisi, "gaji", listKaryawan[5].gaji, "tahun masuk", listKaryawan[5].tahunMasuk);
+  console.log("7.", listKaryawan[6].nama, "bekerja sebagai", listKaryawan[6].posisi, "gaji", listKaryawan[6].gaji, "tahun masuk", listKaryawan[6].tahunMasuk);
+  console.log("8.", listKaryawan[7].nama, "bekerja sebagai", listKaryawan[7].posisi, "gaji", listKaryawan[7].gaji, "tahun masuk", listKaryawan[7].tahunMasuk);
+  console.log("9.", listKaryawan[8].nama, "bekerja sebagai", listKaryawan[8].posisi, "gaji", listKaryawan[8].gaji, "tahun masuk", listKaryawan[8].tahunMasuk);
+  console.log("10.", listKaryawan[9].nama, "bekerja sebagai", listKaryawan[9].posisi, "gaji", listKaryawan[9].gaji, "tahun masuk", listKaryawan[9].tahunMasuk);
 }
 
 daftarKaryawan()
